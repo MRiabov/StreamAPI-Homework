b9178d134608448bc624740fd8d1932e4184977a
MRiabov <maksymriabov2004@gmail.com>
2022-07-12 22:02:51 +0100

    I will do 3 API homeworks in one run, because why litter GitHub with trash projects?

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,17 @@
+import user.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        List<User> users =new ArrayList<>();
+        for (int i = 0; i < 10; i++) users.add(new User("Name"+i,i*8));
+        List<User> sorted = users.stream()
+                .sorted((o1, o2) -> Integer.compare(o2.getAge(), o1.getAge()))
+                .limit(3)
+                .toList();
+        for (User user : sorted) System.out.println(user);
+    }
+
 }
---
+++ src/main/java/user/User.java
@@ -0,0 +1,23 @@
+package user;
+
+public class User {
+    private final int age;
+    private final String name;
+
+    public User(String name,int age) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
