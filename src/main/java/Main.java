aa7b07bfae5defce5f48b3e3e3ba89b37e08d31d
MRiabov <maksymriabov2004@gmail.com>
2022-07-13 19:05:52 +0100

    Well, I feel like i'm done with Stream API. Wish to know soo much more though...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,16 +2,15 @@ import user.User;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class Main {
     public static void main(String[] args) {
         List<User> users =new ArrayList<>();
         for (int i = 0; i < 10; i++) users.add(new User("Name"+i,i*8));
-        users.stream()
-            .sorted((o1,o2)->String.CASE_INSENSITIVE_ORDER.compare(o1.getName(),o2.getName()))
-            .filter((user -> user.getAge()>40))
-            .limit(3)
-            .map(User::getName)
-            .forEach(System.out::println);
+        Optional<User> endsWith3= users.stream()
+                .filter(o->o.getName().endsWith("3"))
+                .findFirst();
+        endsWith3.ifPresentOrElse(System.out::println,() -> System.out.println("404"));
     }
 }
